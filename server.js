dc852c8bfd982030a849099392c564c2b9ad5dd9
Jherico Cocosa <njcocosa@gmail.com>
2020-10-18 12:49:51 +0800

    oct 18

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express')
+const nodemailer = require('nodemailer')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser');
+const path = require('path')
+const { urlencoded } = require('body-parser');
+const { info } = require('console');
+
 const app = express();
 let port = 3000;
 
-app.use(express.static(__dirname + '/public'))
+app.engine('html', exphbs());
+app.set('view engine', 'html');
 
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+
+app.use('/public',express.static(path.join(__dirname,'/public')));
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
 
 app.listen(port, () => {
     console.log("Success to 3000")
